afa5798c281f9534ac264d7c68192814d8ddb538
angelajor <angela.jornalista@gmail.com>
2021-07-02 15:01:42 -0300

    Adicionado HelloManyTimes

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -1,9 +1,14 @@
 public class Exemplo {
-    public static void sayHello(){
+    private static void sayHelloManyTimes(int times) {
+        for (int i = 0; i < times; i++) {
+            sayHello();
+        }
+    }
+    private static void sayHello(){
         System.out.println("Hello, Pandemic World");
     }
 
     public static void main(String[] args) {
-        sayHello();
+        sayHelloManyTimes(5);
     }
 }
