3fa72058c479cbe8a21d083ab56c38a31bf74e69
angelajor <angela.jornalista@gmail.com>
2021-07-02 14:54:08 -0300

    Refatorado o main.

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -1,5 +1,9 @@
 public class Exemplo {
-    public static void main(String[] args) {
+    public static void sayHello(){
         System.out.println("Hello, Pandemic World");
+    }
+
+    public static void main(String[] args) {
+        sayHello();
     }
 }
